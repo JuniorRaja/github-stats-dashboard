0c09fc4639a6f839806ec3d19f34555c18c3348a
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-10-02 21:36:31 +0530

    chore : description length toggle using state

--- components/repoCard.tsx
+++ components/repoCard.tsx
@@ -9,6 +9,17 @@ interface RepositoryCardProps {
 const RepositoryCard: React.FC<RepositoryCardProps> = ({ repository }) => {
   const [isModalOpen, setModalOpen] = useState(false);
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  // Function to toggle the description
+  const toggleDescription = () => setIsExpanded(!isExpanded);
+
+  // Display either truncated or full description
+  const displayedDescription = isExpanded
+    ? repository.description
+    : repository.description.split(" ").slice(0, 15).join(" ") +
+      (repository.description.split(" ").length > 15 ? "..." : "");
+
   return (
     <>
       <div className="p-4 rounded-lg shadow-md bg-gray-200 dark:bg-gray-800">
@@ -16,7 +27,15 @@ const RepositoryCard: React.FC<RepositoryCardProps> = ({ repository }) => {
           {repository.name}
         </h2>
         <p className="text-sm text-gray-700 dark:text-gray-300 mb-4">
-          {repository.description}
+          {displayedDescription}
+          {repository.description.split(" ").length > 20 && (
+            <button
+              onClick={toggleDescription}
+              className="text-blue-500 dark:text-blue-300 hover:underline text-sm"
+            >
+              {isExpanded ? "Show Less" : "Show More"}
+            </button>
+          )}
         </p>
 
         <div className="flex flex-wrap items-center gap-2 mb-4">
@@ -29,15 +48,17 @@ const RepositoryCard: React.FC<RepositoryCardProps> = ({ repository }) => {
           <span className="text-xs font-medium text-gray-600 dark:text-gray-400">
             Stars: {repository.stargazerCount}
           </span>
-          <div className="flex items-center gap-2">
-            <span className="text-sm font-semibold text-gray-900 dark:text-gray-100">
-              {`</> ${repository.primaryLanguage.name}`}
-            </span>
-            <span
-              className="w-3 h-3 rounded-full"
-              style={{ backgroundColor: repository.primaryLanguage.color }}
-            />
-          </div>
+          {repository.primaryLanguage.name !== "Unknown" && (
+            <div className="flex items-center gap-2">
+              <span className="text-sm font-semibold text-gray-900 dark:text-gray-100">
+                {`</> ${repository.primaryLanguage.name}`}
+              </span>
+              <span
+                className="w-3 h-3 rounded-full"
+                style={{ backgroundColor: repository.primaryLanguage.color }}
+              />
+            </div>
+          )}
         </div>
 
         <a
